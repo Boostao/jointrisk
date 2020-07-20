ebace14cfac870b129405a23e91e6b8a8a42757b
Bruno Tremblay <bruno.tremblay@lacapitale.com>
2020-07-20 09:37:28 -0400

    skip test on CI

--- tests/testthat/test-polygons.R
+++ tests/testthat/test-polygons.R
@@ -1,7 +1,12 @@
 context("Polygons creation")
-test_that("Compo are available and same as inforce", {
-  library(extraw)
+test_that("Compo are available", {
   expect_equal(names(compo), c("COTYCONS", "COTYCON2"))
+})
+
+test_that("Compo are same as inforce", {
+  skip_on_ci()
+  library(extraw)
   inforce_compo <- get_compo(c("COTYCONS", "COTYCON2"), inforce = TRUE)
   expect_equal(compo, inforce_compo)
 })
+
