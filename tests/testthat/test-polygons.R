f1b7f5b169977fe1692518d452c753a7f1983b05
Bruno Tremblay <bruno.tremblay@lacapitale.com>
2020-07-20 09:34:04 -0400

    add dep in test

--- tests/testthat/test-polygons.R
+++ tests/testthat/test-polygons.R
@@ -1,5 +1,6 @@
 context("Polygons creation")
 test_that("Compo are available and same as inforce", {
+  library(extraw)
   expect_equal(names(compo), c("COTYCONS", "COTYCON2"))
   inforce_compo <- get_compo(c("COTYCONS", "COTYCON2"), inforce = TRUE)
   expect_equal(compo, inforce_compo)
